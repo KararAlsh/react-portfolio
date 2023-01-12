fb42e72e93a71cd5500c9acacb5626acf2b63bbe
KararAlsh <kararabdulsahib@hotmail.com>
2023-01-12 13:22:10 +0100

    added custom font

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className=' bg-white px-10 '>
         <section className='min-h-screen'>
           <nav className='py-10 mb 12 flex justify-between'>
-            <h1 className=' text-xl'>Karar Alshreese</h1>
+            <h1 className=' text-xl font-burtons'>Karar Alshreese</h1>
             <ul className=' flex items-center'>
               <li>
                 <BsLightbulbFill className='cursor-pointer text-2xl'/>
